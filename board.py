da436470e741b182613843821a24c1f3b0f49b6d
samgiles <samdotgiles@gmail.com>
2019-04-17 14:13:23 -0500

    started modularizing

---
+++ board.py
@@ -0,0 +1,10 @@
+def print():
+	print("_______________")
+
+	for x in range(0,6):   #printing the rows
+		for y in range(0,8): #printing the columns
+				print("|", end=" ")
+			 	#INSERT BOARD DATA HERE
+
+		print()
+	print("_______________")
--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-
-def printBoard():
-	print("_______________")
-
-	for x in range(0,6):   #printing the rows
-		for y in range(0,8): #printing the columns
-				print("|", end=" ")
-			 	#INSERT BOARD DATA HERE
-
-		print()
-	print("_______________")
-
-
-
+import board
 
 if __name__ == '__main__':
 	
-	printBoard()
+	board.print()
