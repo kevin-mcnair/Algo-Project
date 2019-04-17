a46fff64dc3ee827fe76ee149be6b61bae3abfaf
Kevin McNair <kmcnair@smu.edu>
2019-04-17 13:54:24 -0500

    push2

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 
 def printBoard():
+	print("_______________")
 
-	for x in range(0,5):
-		print("\n| ")
+	for x in range(0,6):   #printing the rows
+		for y in range(0,8): #printing the columns
+				print("|", end=" ")
+			 	#INSERT BOARD DATA HERE
+
+		print()
+	print("_______________")
+
+
+
+
+if __name__ == '__main__':
+	
+	printBoard()
