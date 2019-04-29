b5bf95ad7b9e5cdbc9852f8a950e809c14c45c36
samgiles <samdotgiles@gmail.com>
2019-04-28 22:13:14 -0500

    Finished most of the game logic, need to add board functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 class Board:
 
@@ -9,20 +10,21 @@ class Board:
 		self.grid = [[(0,0) for x in range(6)] for y in range(7)]
 		self.gameOver = False
 
-	def addPiece(self, row, col):
+	def addPiece(self, row):
 		print("Add Piece")
 
 	def printBoard(self):
 		print("printBoard")
 
-	def isGameOver(self):
-		return self.gameOver
-
 	def gameIsOver(self):
 		self.gameOver = True
 
 	def checkForWin(self):
-		return False
+		return self.gameOver
+
+	def takeTurn(self, thePlayer):
+		chosenRow = thePlayer.takeTurn()
+		self.addPiece(chosenRow)
 
 class Player:
 
@@ -33,11 +35,18 @@ class Player:
 
 	def takeTurn(self):
 		if self.isComputer:
-			#insert algorithm here bois
-			print("I am a computer")
+			#insert algorithm here bois instead of the random number
+			#have it return the number of the row that it chooses to play in
+			print("Computer's turn...")
+			time.sleep(2)
+			return random.choice([1, 2, 3, 4, 5, 6, 7])
 		else:
 			#get input from user
-			print("I am a user")
+			userInput = input("Your turn! Which row would you like to drop your piece in (1-6)?: ")
+			while userInput != 1 and userInput != 2 and userInput != 3 and userInput != 4 and userInput != 5 and userInput != 6 and userInput != 7:
+				userInput = input("Please enter a row on the board: ")
+			return userInput
+
 
 if __name__ == '__main__':
 
@@ -48,7 +57,9 @@ if __name__ == '__main__':
 	player = Player(False)
 	computer = Player(True)
 
+	print("----------------------")
 	print("Welcome to Connect 4!")
+	print("----------------------")
 	print("Flipping coin to see who goes first...")
 	#generate either true or false, if the coin is true, the computer goes first
 	coin = random.choice([True, False])
@@ -57,27 +68,38 @@ if __name__ == '__main__':
 	#if computer goes first, let the computer take its turn, otherwise don't do anything
 	if coin:
 		#computer takes turn first
-		computer.takeTurn()
+		print("Computer goes first...")
+		time.sleep(.3)
+		theBoard.takeTurn(computer)
+
+	else:
+		print()
+		print("You go first...")
+		print()
 
 	#start while loop with condition gameOver
 	while(theBoard.gameOver == False):
 		#print board
 		theBoard.printBoard()
 		#player takes turn
-		player.takeTurn()
+		theBoard.takeTurn(player)
 		#print board
 		theBoard.printBoard()
 		#check for win
 		gameHasBeenWon = theBoard.checkForWin()
 		#if win, break
 		if gameHasBeenWon:
+			theBoard.gameIsOver()
 			break
 		#otherwise, computer takes turn
-		computer.takeTurn()
+		theBoard.takeTurn(computer)
 		#print board again
-		theBoard.printBoard
+		theBoard.printBoard()
 		#check for win
 		gameHasBeenWon = theBoard.checkForWin()
 		#if win, break
 		if gameHasBeenWon:
+			theBoard.gameIsOver()
 			break
+
+		theBoard.gameIsOver()
