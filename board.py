6db42761ebcc7d76b3fcb3d1d6aed39cbd93170e
Kevin McNair <kmcnair@smu.edu>
2019-05-05 16:36:04 -0500

    board moved from main to board.py

--- board.py
+++ board.py
@@ -1,12 +1,54 @@
-def print():
-	print("_______________")
+class Board:
 
-	for x in range(0,6):   #printing the rows
-		for y in range(0,8): #printing the columns
-				if((x == 5) and (y != 7)):
-					print("|", end="\u0332")
-				else:
-					print("|", end=" ")
-			 	#INSERT BOARD DATA HERE
+	grid = [[]]
+	gameOver = False
+	#this is a list that keeps track of how many pieces are each column
+	#makes sure the pieces get inserted at the "top" of each stack
+	colCounters = []
 
-		print()
+	def __init__(self):
+		self.grid = [[" " for x in range(7)] for y in range(6)]
+		self.gameOver = False
+		self.colCounters = [0,0,0,0,0,0,0]
+
+	def addPiece(self, col, player):
+		#make sure they don't go over the height of the board
+		column = col-1
+
+		if self.colCounters[column]<=6:
+			if player.isComputer:
+				#Computer gets X
+				self.grid[self.colCounters[column]][column] = "X"
+			else:
+				#User gets O
+				self.grid[self.colCounters[column]][column] = "O"
+			#increment the counter for that column
+			self.colCounters[column] = self.colCounters[column] + 1
+			return True
+		else:
+			#only print out the error message for the user
+			if player.isComputer == False:
+				print("Oops! That column is full! Try again.")
+			return False
+
+	def printBoard(self):
+		print("______________")
+		for row in range(len(self.grid), 0, -1):
+			for col in range(len(self.grid[row-1])):
+				print("|" + str(self.grid[row-1][col]), end = '')
+			print("|")
+		print("--------------")
+		print("|1|2|3|4|5|6|7| ")
+
+	def gameIsOver(self):
+		self.gameOver = True
+
+	#returns boolean, false if no 4 pieces found 
+	def checkForWin(self):
+		return self.gameOver
+
+	def takeTurn(self, thePlayer):
+		success = False
+		while success == False:
+			chosenCol = thePlayer.takeTurn()
+			success = self.addPiece(chosenCol, thePlayer)
--- main.py
+++ main.py
@@ -1,61 +1,7 @@
 from __future__ import print_function
 import random
 import time
-
-class Board:
-
-	grid = [[]]
-	gameOver = False
-	#this is a list that keeps track of how many pieces are each column
-	#makes sure the pieces get inserted at the "top" of each stack
-	colCounters = []
-
-	def __init__(self):
-		self.grid = [[" " for x in range(7)] for y in range(6)]
-		self.gameOver = False
-		self.colCounters = [0,0,0,0,0,0,0]
-
-	def addPiece(self, col, player):
-		#make sure they don't go over the height of the board
-		column = col-1
-
-		if self.colCounters[column]<=6:
-			if player.isComputer:
-				#Computer gets X
-				self.grid[self.colCounters[column]][column] = "X"
-			else:
-				#User gets O
-				self.grid[self.colCounters[column]][column] = "O"
-			#increment the counter for that column
-			self.colCounters[column] = self.colCounters[column] + 1
-			return True
-		else:
-			#only print out the error message for the user
-			if player.isComputer == False:
-				print("Oops! That column is full! Try again.")
-			return False
-
-	def printBoard(self):
-		print("______________")
-		for row in range(len(self.grid), 0, -1):
-			for col in range(len(self.grid[row-1])):
-				print("|" + str(self.grid[row-1][col]), end = '')
-			print("|")
-		print("--------------")
-		print("|1|2|3|4|5|6|7| ")
-
-	def gameIsOver(self):
-		self.gameOver = True
-
-	#returns boolean, false if no 4 pieces found 
-	def checkForWin(self):
-		return self.gameOver
-
-	def takeTurn(self, thePlayer):
-		success = False
-		while success == False:
-			chosenCol = thePlayer.takeTurn()
-			success = self.addPiece(chosenCol, thePlayer)
+import board
 
 class Player:
 	
@@ -123,7 +69,7 @@ class Player:
 				#could be a 'heuristic' to always have the computer drop in the center column (4) if they have the first move
 				#According to internet, the only way to win a perfect game is to play that column first... soo optimal?
 				return 4
-
+			return random.choice([1,2,3,4,5,6,7])
 
 			#**************************************************************
 			#IMPORTANT
@@ -161,7 +107,7 @@ class Player:
 if __name__ == '__main__':
 
 	#instantiate board
-	theBoard = Board()
+	theBoard = board.Board()
 
 	#instantiate two new player objects
 	player = Player(False)
