6e84f31b1b7fd6575991d9866a3cf4488c34a70a
samgiles <samdotgiles@gmail.com>
2019-05-06 16:13:49 -0500

    The AI now has an algorithm

--- Connect4AI.py
+++ Connect4AI.py
@@ -1,4 +1,24 @@
 import random
+import board
+
 class AI: 
-    def chosenColumn(self):
-        return random.choice([1,2,3,4,5,6,7])
+    def chosenColumn(self, theBoard):
+
+        #check to see if there are a lot of 0's in the first row
+        totalOs = 0
+        for col in range(0, 5):
+            totalOs = 0
+            for row in range(0, 6):
+                if theBoard.grid[row][col] == ' ':
+                    continue
+                if theBoard.grid[row][col].character == "O":
+                    totalOs = totalOs + 1
+                else:
+                    totalOs = 0
+            print('Total consecutive Os in col ' + str(col))
+            print(totalOs)
+            if totalOs >= 3:
+                print("Making move based on Os" + str(col))
+                return col
+
+        return random.choice([0,1,2,3,4,5,6])
