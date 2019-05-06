3235dc421d7a926aba983c91600dd85c0bafc50b
samgiles <samdotgiles@gmail.com>
2019-05-06 16:52:55 -0500

    added a try except

--- Connect4AI.py
+++ Connect4AI.py
@@ -4,21 +4,37 @@ import board
 class AI: 
     def chosenColumn(self, theBoard):
 
-        #check to see if there are a lot of 0's in the first row
-        totalOs = 0
-        for col in range(0, 5):
+        #first checks to see if it can win
+        try:
+            for y in range(0, 5):
+                for x in range(0, 6):
+                    if theBoard.grid[x][y] == ' ':
+                        continue
+                    if theBoard.grid[x][y].character == "X":
+                        if str(theBoard.grid[x][y+1]) == "X":
+                            if str(theBoard.grid[x][y+2]) == "X":
+                                return y+3
+                        elif str(theBoard.grid[x][y-1]) == "X":
+                            if str(theBoard.grid[x][y-2]) == "X":
+                                return y-3
+                        elif str(theBoard.grid[x+1][y]) == "X":
+                            if str(theBoard.grid[x+2][y]) == "X":
+                                return y
+                
+        except IndexError:
+            print("Out of bounds")
+            #this is a simple blocking algorithm
             totalOs = 0
-            for row in range(0, 6):
-                if theBoard.grid[row][col] == ' ':
-                    continue
-                if theBoard.grid[row][col].character == "O":
-                    totalOs = totalOs + 1
-                else:
-                    totalOs = 0
-            print('Total consecutive Os in col ' + str(col))
-            print(totalOs)
-            if totalOs >= 3:
-                print("Making move based on Os" + str(col))
-                return col
+            for col in range(0, 5):
+                totalOs = 0
+                for row in range(0, 6):
+                    if theBoard.grid[row][col] == ' ':
+                        continue
+                    if theBoard.grid[row][col].character == "O":
+                        totalOs = totalOs + 1
+                    else:
+                        totalOs = 0
+                if totalOs >= 3:
+                    return col
 
-        return random.choice([0,1,2,3,4,5,6])
+            return random.choice([0,1,2,3,4,5,6])
